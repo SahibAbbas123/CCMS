56fa51f3961dc0d058732e81fc9ba5c0e45eaf5b
Sahib <chyabbas34@gmail.com>
2024-03-06 01:09:30 +0600

    Code refactored

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Intro from './components/Intro'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Navbar></Navbar>
+    <Intro></Intro>
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,11 +18,6 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-    <div className="container mt-4">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula massa ut lectus sodales, nec interdum velit ultricies. Fusce rhoncus hendrerit risus, at tempus tortor vehicula sit amet. Duis nec magna convallis, sagittis lacus ut, tempus justo. Nullam sit amet turpis urna. Nulla ac finibus sapien. In hac habitasse platea dictumst. Integer posuere quam vitae metus feugiat, a fermentum libero volutpat. Vivamus sit amet mi id urna dignissim mattis. Nam a purus ligula. Vestibulum non diam nec nulla pharetra dignissim. Nulla facilisi.
-    </p>
-  </div>
   </>
   );
 }
